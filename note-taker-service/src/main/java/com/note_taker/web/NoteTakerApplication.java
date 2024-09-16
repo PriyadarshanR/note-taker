cab7e32d74d6393a54b68f7f78d7a7bd5a18bb2b
PriyadarshanR <dedarsh2000@gmail.com>
2024-09-16 17:08:45 +0530

    db config change

--- note-taker-service/src/main/java/com/note_taker/web/NoteTakerApplication.java
+++ note-taker-service/src/main/java/com/note_taker/web/NoteTakerApplication.java
@@ -2,6 +2,7 @@ package com.note_taker.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NoteTakerApplication {
@@ -9,5 +10,4 @@ public class NoteTakerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NoteTakerApplication.class, args);
 	}
-
 }
